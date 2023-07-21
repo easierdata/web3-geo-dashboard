df40b7322fb837f6620f908d00d623ec06296be2
John Solly <jsolly@umd.edu>
2023-07-20 20:47:38 -0700

    wip

--- src/lib/map.svelte
+++ src/lib/map.svelte
@@ -4,7 +4,7 @@
 
 	let map: Map;
 
-	onMount(() => {
+	onMount(async () => {
 		if (!import.meta.env.VITE_MAPBOX_TOKEN) {
 			throw new Error('MAPBOX_TOKEN is required');
 		}
@@ -16,6 +16,24 @@
 			style: 'mapbox://styles/mapbox/streets-v11',
 			center: [-122.486052, 37.830348],
 			zoom: 15
+		});
+
+		map.on('load', function () {
+			map.addSource('US_STATES_OUTLINE', {
+				type: 'vector',
+				url: 'mapbox://<tileset_id>'
+			});
+
+			map.addLayer({
+				'id': 'US_STATES_OUTLINE-layer',
+				'type': 'fill', // You can modify this based on your specific needs
+				'source': 'US_STATES_OUTLINE',
+				'source-layer': '<layer_name>', 
+				'paint': {
+					'fill-color': '#888888',
+					'fill-opacity': 0.4
+				}
+			});
 		});
 	});
 
