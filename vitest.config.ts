953872421dba2a0d8611401f49ed466f1574f85d
John Solly <jsolly@umd.edu>
2023-07-27 12:53:00 -0700

    attempt to fix errors before finishing commit

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
 	plugins: [svelte({ hot: !process.env.VITEST })],
 	test: {
-		globals: true,
+		coverage: {
+			provider: 'v8',
+			reporter: ['lcov', ['text', { skipFull: true }]]
+		},
 		environment: 'jsdom'
 	}
 });
