73182ae120b15a6b1a7907c0a6662d06d113f421
matthewnanas <spazeplayz@gmail.com>
2023-08-05 11:26:54 -0400

    Connect to browser

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,34 @@
 <script lang="ts">
+	import { ethers } from 'ethers'
 	export let title = 'Title According to Layout Component';
 	import '../global.css';
+	import { onMount } from 'svelte';
+
+	let provider: ethers.BrowserProvider;
+	let connectedAccount = "";
+	onMount(() => {
+		provider = new ethers.BrowserProvider(window.ethereum)
+	})
+
+	async function connectWallet() {
+		if (window.ethereum) {
+			await window.ethereum.request({ method: 'eth_requestAccounts' })
+			const signer = await provider.getSigner();
+			connectedAccount = await signer.getAddress();
+		} else {
+			alert('Metamask not detected!');
+		}
+	}
 </script>
 
 <header>
 	<h1>{title}</h1>
+	<div class="MetamaskContainer">
+		<button on:click={connectWallet} class="ConnectButton">Connect wallet</button>
+		{#if connectedAccount != ""}
+			<div>Connected</div>
+		{/if}
+	</div>
 </header>
 
 <main>
@@ -15,3 +39,13 @@
 	<a href="/prop_example">Prop Example</a>
 	<img src="/easierLogo.png" alt="Easier logo" width="auto" height="100" />
 </footer>
+
+<style>
+	.MetamaskContainer {
+		position: absolute;
+		right: 0;
+		margin-right: 20px;
+		margin-top: -50px;
+		float: right;
+	}
+</style>
