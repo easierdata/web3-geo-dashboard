ce43446f2f5f27c12b7f6edbd8f1a34070eb659e
John Solly <jsolly@umd.edu>
2023-08-14 14:41:17 -0700

    Add doc comment about ISO strings

--- src/types.ts
+++ src/types.ts
@@ -9,7 +9,7 @@ interface CustomFeatureProperties {
 	ROW: string;
 	PATH: string;
 	CID: string;
-	DATE: string;
+	DATE: string; // Expected in ISO format, e.g., '2023-08-12T14:30:00Z'
 	IPFS_NODES: number;
 	FIL_DEALS: number;
 	ON_S3: boolean;
